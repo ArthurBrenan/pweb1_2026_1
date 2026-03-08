158d4fc293c384323bb48b19886416bac81226bf
ArthurBrenan <arthur.bdb@aluno.ifsc.edu.br>
2026-03-08 18:57:00 -0300

    commit 1 08/03
    alterações

--- Exercícios/index.php
+++ Exercícios/index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Exercícios Práticos 02</title>
 </head>
+
 <body>
-    
+
+<h1>Exercícios Práticos 02</h1>
+
+<ul>
+    <li><a href="exercicio2.1(matricula).html">Exercício 1</a></li>
+    <li><a href="exercicio2.2.html">Exercício 2</a></li>
+    <li><a href="exercicio2.3.html">Exercício 3</a></li>
+</ul>
+
 </body>
 </html>
